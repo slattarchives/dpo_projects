408bc9c1d882708197fbebeacddf6ed23bae4dd1
https://git.21-school.ru/andersoc <olga@DESKTOP-C9CBEUU.localdomain>
2026-03-04 13:58:07 +0300

    adding trial for 12prac

---
+++ practice12/prac12.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', function () {
+  //№1: Генерация списка
+  const containeR = document.getElementById('containeR');
+  const addUserBtn = document.getElementById('addUserBtn');
+  let counter = 0;
+
+  const title = document.createElement('h1');
+  title.textContent = 'Список пользователей';
+  title.style.color = '#FF8AAE';
+  title.style.marginBottom = '1rem';
+
+  addUserBtn.addEventListener('click', function () {
+    counter = counter + 1;
+  });
+
+  //№2: Работа с селекторами
+  const textElements = document.querySelectorAll('.text');
+  console.log('Найдено элементов с классом "text":', textElements.length);
+
+  const specialElement = document.querySelector('.special');
+  if (specialElement) {
+    specialElement.classList.add('red-text');
+  }
+
+  if (textElements.length >= 3) {
+    textElements[2].classList.add('bg-green'); // индекс 2 = третий элемент
+  }
+
+  const container = document.getElementById('container');
+  if (container) {
+    container.classList.add('border-container');
+  }
+
+
+  //№3: Data-атрибуты и вычисления
+  const items = document.querySelectorAll('.item');
+  console.log('Найдено товаров:', items.length);
+
+  const activeItem = document.querySelector('.item.active');
+  if (activeItem) {
+    activeItem.classList.add('highlight');
+  }
+
+  let total = 0;
+  let maxPrice = 0;
+  let maxItem = null;
+
+  items.forEach(item => {
+    const price = Number(item.dataset.price);
+    total += price;
+    
+    if (price > maxPrice) {
+      maxPrice = price;
+      maxItem = item;
+    }
+  });
+
+  console.log('Суммарная стоимость:', total, '₽');
+  console.log('Самый дорогой товар:', maxItem ? maxItem.textContent : 'не найден', '—', maxPrice, '₽');
+
+  const resultDiv = document.getElementById('calcResult');
+  resultDiv.innerHTML = `
+    Суммарная стоимость: <strong>${total} ₽</strong><br>
+    Самый дорогой товар: <strong>${maxItem ? maxItem.textContent : '—'}</strong> (${maxPrice} ₽)
+  `;
+});
